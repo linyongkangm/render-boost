ce68316cd2b7aab5fae4ad5fbfc0e0fe19aa6390
linyongkangm <linyongkangm@foxmail.com>
2023-01-08 19:30:17 +0800

    feat: alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, AliasOptions } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import pageageJson from './package.json';
+import tsconfig from './tsconfig.json';
+import path from 'path';
 
+const alias: AliasOptions = {};
+const baseUrl = tsconfig.compilerOptions.baseUrl;
+const paths = tsconfig.compilerOptions.paths;
+Object.entries(paths).forEach(([_key, _value]) => {
+  const val = _value[0];
+  const key = _key.replace('/*', '');
+  const value = path.resolve(__dirname, baseUrl, val?.replace('/*', ''));
+  alias[key] = value;
+});
 export default defineConfig({
   base: `/${pageageJson.name}`,
   plugins: [solidPlugin()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias,
   },
   build: {
     target: 'es2015',
