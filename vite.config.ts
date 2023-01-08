9428bad80d088616cbb8dc4107d889860ce9ade0
linyongkangm <linyongkangm@foxmail.com>
2023-01-08 16:02:57 +0800

    feat: 修改base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import pageageJson from './package.json';
 
 export default defineConfig({
+  base: `/${pageageJson.name}`,
   plugins: [solidPlugin()],
   server: {
     port: 3000,
