35bd3c62133cc37a6aee8aa1be4e9d909df72c39
linyongkangm <linyongkangm@foxmail.com>
2023-01-08 16:53:40 +0800

    feat: 修改一下

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,12 @@ const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+        <img src={logo} class={styles.logo} alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
+        <a class={styles.link} href='https://github.com/solidjs/solid' target='_blank' rel='noopener noreferrer'>
+          Learn Solid 1111
         </a>
       </header>
     </div>
